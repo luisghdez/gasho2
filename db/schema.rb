e53e9107e2c1dc6de2221128bc97180598d52d31
Luis Hernandez <1gluishernandez@gmail.com>
2022-05-30 06:19:39 -0600

    basic models. sports, routines, stretches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_120422) do
+ActiveRecord::Schema.define(version: 2022_05_30_121619) do
+
+  create_table "routines", force: :cascade do |t|
+    t.string "name"
+    t.integer "duration"
+    t.string "description"
+    t.integer "sport_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sport_id"], name: "index_routines_on_sport_id"
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sports_on_user_id"
+  end
+
+  create_table "stretches", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "routine_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["routine_id"], name: "index_stretches_on_routine_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +51,7 @@ ActiveRecord::Schema.define(version: 2022_05_30_120422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "routines", "sports"
+  add_foreign_key "sports", "users"
+  add_foreign_key "stretches", "routines"
 end
