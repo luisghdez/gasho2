2243f159cf6439ef0d38635e8e8b0d1f634b626a
Jose <loayzajoseignacio3@gmail.com>
2022-06-03 15:30:17 +0200

    Disable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_143946) do
+ActiveRecord::Schema.define(version: 2022_06_03_104849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2022_06_02_143946) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "difficulty"
+    t.string "photo"
   end
 
   create_table "stretches_routines", force: :cascade do |t|
